942faa1f087b8298984eaf96f0464c077c552259
Laura Antonache <lauraantonache@gmail.com>
2015-06-24 16:01:32 +0300

    Not working yet! Need to change the reading

--- Python/tests_manager.py
+++ Python/tests_manager.py
@@ -4,28 +4,50 @@ import sys
 import csv
 
 database = []
-
-
+test = []
+crt_test = ""
 with open('stats.csv', 'rb') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter='"', quotechar='|')
-	for row in spamreader:
-		item = ', '.join(row)
-		tokens = item.strip().split(",")
-		entry = {
-				'test_name':tokens[1],
-				'school_name':tokens[5],
-				'student_name':tokens[8],
-				'grade':int(tokens[11])
-		}
-		print entry
-		database.append(entry)
+    spamreader = csv.reader(csvfile, delimiter='"', quotechar='|')
+    for row in spamreader:
+        item = ', '.join(row)
+        tokens = item.strip().split(",")
+        if crt_test == tokens[1]:
+            entry = {
+                'test_name': tokens[1],
+                'school_name': tokens[5],
+                'student_name': tokens[8],
+                'grade': tokens[11]
+            }
+            test.append(entry)
+        else:
+            #print test
+            database.append(test)
+            crt_test = tokens[1]
+            entry = {
+                'test_name': tokens[1],
+                'school_name': tokens[5],
+                'student_name': tokens[8],
+                'grade': tokens[11]
+            }
+            test = [entry]
+
+#print database[1]
 
 
+for i in range (len(database)):
+    max = 0
+    max_item = {
+        'test_name': "none",
+        'school_name': "none",
+        'student_name': "none",
+        'grade': "0"
+    }
+    #print database[i]
+    for item in database[i]:
+        print item
+        crt_grade = int(item['grade'])
+        if crt_grade > max:
+            max = crt_grade
+            max_item = item
+    print max
 
-
-
-"""with open('stats.csv') as csvfile:
-	reader = csv.DictReader(csvfile)
-	for row in reader:
-		print(row['test_name'], row['person'], row['grade'])
-"""
